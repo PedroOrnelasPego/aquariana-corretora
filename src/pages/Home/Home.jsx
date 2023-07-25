4eb034133bb2cf24ff7a366df87b0b2907dcf496
unknown <pedroornelaspego@gmail.com>
2023-07-25 17:31:10 -0300

    last update

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,10 @@
 import Header from "../../components/Header";
+import { motion } from "framer-motion";
+import { ImMobile, ImTruck } from "react-icons/im";
+import { FaCar, FaMotorcycle, FaLaptop, FaCamera } from "react-icons/fa";
+import { BiSolidBuildingHouse } from "react-icons/bi";
+import { PiAirplaneTiltFill } from "react-icons/pi";
+import { BsFillHeartPulseFill, BsFillHouseDoorFill } from "react-icons/bs";
 import "./Home.scss";
 
 const Home = () => {
@@ -14,68 +20,172 @@ const Home = () => {
           </h1>
         </div>
         <div className="containerr">
-          <div className="container_seguros">
-            <div className="card_seguros">
-              <div className="imgBx"></div>
-              <div className="content_seguros">pedro</div>
-            </div>
-          </div>
-          <div className="container_seguros">
-            <div className="card_seguros">
-              <div className="imgBx"></div>
-              <div className="content_seguros">pedro</div>
-            </div>
-          </div>
-          <div className="container_seguros">
-            <div className="card_seguros">
-              <div className="imgBxr"></div>
-              <div className="content_seguros">pedro</div>
-            </div>
-          </div>
-          <div className="container_seguros">
-            <div className="card_seguros">
-              <div className="imgBx"></div>
-              <div className="content_seguros">pedro</div>
-            </div>
-          </div>
-          <div className="container_seguros">
-            <div className="card_seguros">
-              <div className="imgBx"></div>
-              <div className="content_seguros">pedro</div>
-            </div>
-          </div>
-          <div className="container_seguros">
-            <div className="card_seguros">
-              <div className="imgBxr"></div>
-              <div className="content_seguros">pedro</div>
-            </div>
-          </div>
-          <div className="container_seguros">
-            <div className="card_seguros">
-              <div className="imgBx"></div>
-              <div className="content_seguros">pedro</div>
-            </div>
-          </div>
-          <div className="container_seguros">
-            <div className="card_seguros">
-              <div className="imgBx"></div>
-              <div className="content_seguros">pedro</div>
-            </div>
-          </div>
-          <div className="container_seguros">
-            <div className="card_seguros">
-              <div className="imgBx"></div>
-              <div className="content_seguros">pedro</div>
-            </div>
-          </div>
-          <div className="container_seguros">
-            <div className="card_seguros">
-              <div className="imgBx"></div>
-              <div className="content_seguros">pedro</div>
-            </div>
-          </div>
+          <a href="" target="_blank" rel="noopener noreferrer">
+            <motion.div
+              className="card_seguros"
+              whileHover={{ scale: 1.2 }}
+              whileTap={{ scale: 0.8 }}
+            >
+              <BiSolidBuildingHouse size={90} />
+              <span>Seguro Condominio</span>
+            </motion.div>
+          </a>
+          <a
+            href="https://wwws.portoseguro.com.br/vendaonline/residencia/home.ns?cod=dd253b6d72c34073bfc776d870ed910d&utm_source=40418J&utm_medium=geradorLinks&utm_campaign=GeradordeLinks_M279YJ&utm_content=AQUARIANA_CORRETORA_E_ADIMINISTRADORA&origem=BELO_HORIZONTE"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <motion.div
+              className="card_seguros"
+              whileHover={{ scale: 1.2 }}
+              whileTap={{ scale: 0.8 }}
+            >
+              <BsFillHouseDoorFill size={90} />
+              <span>Seguro Casa</span>
+            </motion.div>
+          </a>
+          <a
+            href="http://porto.vc/RAMOS_ELEMENTARES_40418J"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <motion.div
+              className="card_seguros"
+              whileHover={{ scale: 1.2 }}
+              whileTap={{ scale: 0.8 }}
+            >
+              <ImMobile size={90} />
+              <span>Celulares Novos</span>
+            </motion.div>
+          </a>
+          <a
+            href="https://wwws.portoseguro.com.br/vendaonline/automovel/home.ns?cod=75ccc1312d2c4b699389930b79d97c55&utm_source=40418J&utm_medium=geradorLinks&utm_campaign=GeradordeLinks_M279YJ&utm_content=AQUARIANA_CORRETORA_E_ADIMINISTRADORA&origem=BELO_HORIZONTE"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <motion.div
+              className="card_seguros"
+              whileHover={{ scale: 1.2 }}
+              whileTap={{ scale: 0.8 }}
+            >
+              <div className="dual_seguros">
+                <FaCar size={60} />
+                <ImTruck size={60} />
+              </div>
+              <span>Seguro de Veículos</span>
+            </motion.div>
+          </a>
+          <a
+            href="https://wwws.portoseguro.com.br/vendaonline/moto/home.ns?cod=94ce9d65e9f64c52b55e8b177bff9ede&utm_source=40418J&utm_medium=geradorLinks&utm_campaign=GeradordeLinks_M279YJ&utm_content=AQUARIANA_CORRETORA_E_ADIMINISTRADORA&origem=BELO_HORIZONTE"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <motion.div
+              className="card_seguros"
+              whileHover={{ scale: 1.2 }}
+              whileTap={{ scale: 0.8 }}
+            >
+              <FaMotorcycle size={90} />
+              <span>Seguro de Motocicletas</span>
+            </motion.div>
+          </a>
+          <a
+            href="https://wwws.portoseguro.com.br/vendaonline/vidamaissimples/home.ns?cod=598a5d23e43045e1973f4cb3d806c817&utm_campaign=GeradordeLinks_M279YJ&utm_content=AQUARIANA_CORRETORA_E_ADIMINISTRADORA&origem=BELO_HORIZONTE"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <motion.div
+              className="card_seguros"
+              whileHover={{ scale: 1.2 }}
+              whileTap={{ scale: 0.8 }}
+            >
+              <BsFillHeartPulseFill size={90} />
+              <span>Seguro de Vida</span>
+            </motion.div>
+          </a>
+          <a href="" target="_blank" rel="noopener noreferrer">
+            <motion.div
+              className="card_seguros"
+              whileHover={{ scale: 1.2 }}
+              whileTap={{ scale: 0.8 }}
+            >
+              <div className="dual_seguros">
+                <FaLaptop size={60} />
+                <FaCamera size={60} />
+              </div>
+
+              <span>Equipamentos Portáteis</span>
+            </motion.div>
+          </a>
+          <a
+            href="https://wwws.portoseguro.com.br/vendaonline/viagem/home.ns?cod=2d11f370d2a34415b4d3f63149cbf9d2&utm_source=40418J&utm_medium=geradorLinks&utm_campaign=GeradordeLinks_M279YJ&utm_content=AQUARIANA_CORRETORA_E_ADIMINISTRADORA&origem=BELO_HORIZONTE"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <motion.div
+              className="card_seguros"
+              whileHover={{ scale: 1.2 }}
+              whileTap={{ scale: 0.8 }}
+            >
+              <div className="dual_seguros">
+                <PiAirplaneTiltFill size={90} />
+              </div>
+              <span>Viagem</span>
+            </motion.div>
+          </a>
+          {/* <a href="" target="_blank" rel="noopener noreferrer">
+            <motion.div
+              className="card_seguros"
+              whileHover={{ scale: 1.2 }}
+              whileTap={{ scale: 0.8 }}
+            >
+              <div className="dual_seguros"></div>
+              <span>Empresarial</span>
+            </motion.div>
+          </a>
+          <a href="" target="_blank" rel="noopener noreferrer">
+            <motion.div
+              className="card_seguros"
+              whileHover={{ scale: 1.2 }}
+              whileTap={{ scale: 0.8 }}
+            >
+              <div className="dual_seguros"></div>
+              <span>SEGURO PET ( ANIMAIS )</span>
+            </motion.div>
+          </a>
+          <a href="" target="_blank" rel="noopener noreferrer">
+            <motion.div
+              className="card_seguros"
+              whileHover={{ scale: 1.2 }}
+              whileTap={{ scale: 0.8 }}
+            >
+              <div className="dual_seguros"></div>
+              <span>Seguro de Cargas</span>
+            </motion.div>
+          </a>
+          <a href="" target="_blank" rel="noopener noreferrer">
+            <motion.div
+              className="card_seguros"
+              whileHover={{ scale: 1.2 }}
+              whileTap={{ scale: 0.8 }}
+            >
+              <div className="dual_seguros"></div>
+              <span>AGRO/ SAFRAS</span>
+            </motion.div>
+          </a>
+          <a href="" target="_blank" rel="noopener noreferrer">
+            <motion.div
+              className="card_seguros"
+              whileHover={{ scale: 1.2 }}
+              whileTap={{ scale: 0.8 }}
+            >
+              <div className="dual_seguros"></div>
+              <span>Máquinas</span>
+            </motion.div>
+          </a> */}
         </div>
       </div>
+      
     </div>
   );
 };
