e36a2140ccfe2908f18aa99031db3b99f056597c
unknown <pedroornelaspego@gmail.com>
2023-07-31 21:53:53 -0300

    cards imoveis update

--- src/pages/Imoveis/Aluguel/Aluguel.jsx
+++ src/pages/Imoveis/Aluguel/Aluguel.jsx
@@ -3,6 +3,7 @@ import { useSpring, animated } from "react-spring";
 import { Modal, Button } from "react-bootstrap";
 import "./Aluguel.scss";
 import casa1 from "../../../assets/imoveis/aluguel/casa_jardim_montanhes.jpeg";
+import casa2 from "../../../assets/imoveis/aluguel/casa_teste.jpg";
 
 const imoveisAluguelData = [
   {
@@ -10,30 +11,43 @@ const imoveisAluguelData = [
     title: "Jardim Montanhês",
     description: [
       "Casa de dois pavimentos independentes, sendo duas residências com 03 barracões fundos.",
+
       "01 vaga de garagem livre e coberta que pertence ao primeiro pavimento.",
+
       "Ideal para investidor, com renda aproximadamente de aluguel de R$4.000,00 mensalmente.",
     ],
     imageUrl: casa1,
   },
   {
     id: 2,
-    title: "Jardim Montanfsdfsdfhês",
+    title: "Casa teste 2",
     description: [
       "Casa de dois pavimentos independentes, sendo duas residências com 03 barracões fundos.",
+
       "01 vaga de garagem livre e coberta que pertence ao primeiro pavimento.",
+
       "Ideal para investidor, com renda aproximadamente de aluguel de R$4.000,00 mensalmente.",
     ],
-    imageUrl: casa1,
+    imageUrl: casa2,
   },
   // Adicione outros objetos de imóveis aqui...
 ];
 
+const MAX_DESCRIPTION_LENGTH = 200; // Tamanho máximo da descrição
+
 const Aluguel = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedImovel, setSelectedImovel] = useState(null);
   const [hoveredCard, setHoveredCard] = useState(null);
 
-  const handleCardClick = () => {
+  const handleCardClick = (id) => {
+    const imovel = imoveisAluguelData.find((imovel) => imovel.id === id);
+    setSelectedImovel(imovel);
     setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
   };
 
   const handleMouseEnter = (id) => {
@@ -49,12 +63,8 @@ const Aluguel = () => {
     // eslint-disable-next-line react-hooks/rules-of-hooks
     return useSpring({
       transform: isHovered ? "scale(1.1)" : "scale(1)",
-      cursor: "pointer",
+      zIndex: isHovered ? 1 : 0,
     });
-  };
-
-  const closeModal = () => {
-    setIsModalOpen(false);
   };
 
   return (
@@ -73,54 +83,55 @@ const Aluguel = () => {
           </h1>
         </div>
 
-        <div className="flex justify-center items-center mt-20 flex-wrap">
-          {imoveisAluguelData.map((imovel) => (
-            <animated.div
-              key={imovel.id}
-              className="card_imoveis bg-white shadow-lg rounded-lg p-4 max-w-sm m-2"
-              onMouseEnter={() => handleMouseEnter(imovel.id)}
-              onMouseLeave={handleMouseLeave}
-              style={{
-                ...getCardSpring(imovel.id),
-                zIndex: hoveredCard === imovel.id ? 1 : 0,
-              }}
-              onClick={() => handleCardClick(imovel)}
-            >
-              <div className="card__image-container">
-                <img
-                  src={imovel.imageUrl}
-                  alt={imovel.title}
-                  className="card__image"
-                />
-              </div>
-              <div className="card__content">
-                <h3 className="text-xl font-bold mb-2">{imovel.title}</h3>
-                <hr className="my-2" />
-                <p className="text-gray-600">{imovel.description}</p>
-              </div>
-            </animated.div>
-          ))}
+        <div className="gap-2.5 flex justify-center items-center m-20 flex-wrap">
+          {imoveisAluguelData.map((imovel) => {
+            const truncatedDescription = imovel.description
+              .join(" ")
+              .substring(0, MAX_DESCRIPTION_LENGTH);
+            const description =
+              imovel.description.join(" ").length > MAX_DESCRIPTION_LENGTH
+                ? truncatedDescription + "..."
+                : imovel.description.join(" ");
+
+            return (
+              <animated.div
+                key={imovel.id}
+                className="card_imoveis bg-white shadow-lg rounded-lg p-4 max-w-sm m-2 cursor-pointer"
+                onClick={() => handleCardClick(imovel.id)}
+                onMouseEnter={() => handleMouseEnter(imovel.id)}
+                onMouseLeave={handleMouseLeave}
+                style={getCardSpring(imovel.id)}
+              >
+                <div className="card_imoveis_content">
+                  <img
+                    src={imovel.imageUrl}
+                    alt={imovel.title}
+                    className="card_imoveis_content__image"
+                  />
+                </div>
+                <div className="card__content">
+                  <h3 className="text-xl font-bold mb-2">{imovel.title}</h3>
+                  <hr className="my-2" />
+                  <p className="text-gray-600">{description}</p>
+                </div>
+              </animated.div>
+            );
+          })}
         </div>
       </div>
-      <Modal show={isModalOpen} onHide={closeModal}>
+      <Modal show={isModalOpen} onHide={closeModal} centered>
         <Modal.Header closeButton>
-          <Modal.Title>
-            {hoveredCard !== null && imoveisAluguelData[hoveredCard - 1].title}
-          </Modal.Title>
+          <Modal.Title>{selectedImovel && selectedImovel.title}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          {hoveredCard !== null &&
-            imoveisAluguelData[hoveredCard - 1].description.map(
-              (item, index) => <p key={index}>{item}</p>
-            )}
+          {selectedImovel &&
+            selectedImovel.description.map((item, index) => (
+              <p key={index}>{item}</p>
+            ))}
         </Modal.Body>
         <Modal.Footer>
-          <Button
-            className="text-black"
-            variant="secondary"
-            onClick={closeModal}
-          >
-            Close
+          <Button variant="secondary" onClick={closeModal}>
+            Fechar
           </Button>
         </Modal.Footer>
       </Modal>
