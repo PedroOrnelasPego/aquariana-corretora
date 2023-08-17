8cb81a22e56dd5d266673f654fd15abde3014494
unknown <pedroornelaspego@gmail.com>
2023-08-17 19:02:28 -0300

    update css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,12 +3,14 @@ import "./Header.scss";
 const Header = () => {
   return (
     <header className="masthead">
-        <div className="relative flex items-center flex-col justify-center text-center text-white font-bold text-shadow ">
-          <h1 className="header_title text-6xl md:text-7xl lg:text-8xl xl:text-8xl">
+      <div className="home_header">
+        <div className="relative flex items-center flex-col justify-center text-center text-white font-bold">
+          <h1 className="home_header__title text-6xl md:text-7xl lg:text-8xl xl:text-8xl">
             AQUARIANA
           </h1>
           <h3 className="header_title">Corretora & Administradora</h3>
         </div>
+      </div>
     </header>
   );
 };
