536f875d44f879454428a3432d14f22a10789f32
unknown <pedroornelaspego@gmail.com>
2023-08-17 20:51:27 -0300

    analytics test

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
+import { Analytics } from '@vercel/analytics/react';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router>
-      <AppRoutes />
-    </Router>
+    <Analytics>
+      <Router>
+        <AppRoutes />
+      </Router>
+    </Analytics>
   </React.StrictMode>
 );
