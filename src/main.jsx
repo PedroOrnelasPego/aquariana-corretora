4e719219183a5bcf7f58b16e31e4fc0f2e9db8ba
unknown <pedroornelaspego@gmail.com>
2023-08-17 21:04:04 -0300

    update analitics

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
-import { Analytics } from '@vercel/analytics/react';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Analytics>
-      <Router>
-        <AppRoutes />
-      </Router>
-    </Analytics>
+    <Router>
+      <AppRoutes />
+    </Router>
   </React.StrictMode>
 );
